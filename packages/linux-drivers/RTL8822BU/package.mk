b44f9c25a630360ed39176476231c78d4c365be9
Flavius Georgescu <pretoriano.mp@gmail.com>
2019-09-21 02:38:05 +0300

    Fixed driver issue on kernel 5.3.x

--- packages/linux-drivers/RTL8822BU/package.mk
+++ packages/linux-drivers/RTL8822BU/package.mk
@@ -3,16 +3,16 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="RTL8822BU"
-PKG_VERSION="b655ba8114d7def7e6181bfe4635fbb831e2b8d6"
-PKG_SHA256="bbb6dab43a0e21654a7796ce07796677bbfda322d74b6a55dd08e3bde4953208"
+PKG_VERSION="451073bcf1a717bd62439b68815b1541be16f2da"
+PKG_SHA256="db04383eb54c57fa41deedfa52ae66c8764ee3cd35585d41da21c40c2efc109e"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/cilynx/rtl88x2BU_WiFi_linux_v5.3.1_27678.20180430_COEX20180427-5959"
-PKG_URL="https://github.com/cilynx/rtl88x2BU_WiFi_linux_v5.3.1_27678.20180430_COEX20180427-5959/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/pretoriano80/RTL8822BU"
+PKG_URL="https://github.com/pretoriano80/RTL8822BU/archive/$PKG_VERSION.tar.gz"
 
 
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
-PKG_LONGDESC="Realtek RTL8822BU Linux 4.x driver"
+PKG_LONGDESC="Realtek RTL8822BU Linux 5.x driver"
 PKG_IS_KERNEL_PKG="yes"
 
 pre_make_target() {
