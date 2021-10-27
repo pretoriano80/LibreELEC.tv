da7350afbc54482866a4dbfa69271c14507289e9
SupervisedThinking <supervisedthinking@gmail.com>
2021-10-27 13:59:02 +0200

    u-boot-tools: build known good v2021.07 for Amlogic

--- packages/tools/u-boot-tools/package.mk
+++ packages/tools/u-boot-tools/package.mk
@@ -2,13 +2,19 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="u-boot-tools"
-PKG_VERSION="2021.10"
-PKG_SHA256="cde723e19262e646f2670d25e5ec4b1b368490de950d4e26275a988c36df0bd4"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.denx.de/wiki/U-Boot"
 PKG_URL="http://ftp.denx.de/pub/u-boot/u-boot-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_HOST="ccache:host bison:host flex:host openssl:host pkg-config:host"
 PKG_LONGDESC="Das U-Boot is a cross-platform bootloader for embedded systems."
+
+if [ ${PROJECT} = "Amlogic" ]; then
+  PKG_VERSION="2021.07"
+  PKG_SHA256="312b7eeae44581d1362c3a3f02c28d806647756c82ba8c72241c7cdbe68ba77e"
+else
+  PKG_VERSION="2021.10"
+  PKG_SHA256="cde723e19262e646f2670d25e5ec4b1b368490de950d4e26275a988c36df0bd4"
+fi
 
 make_host() {
   make qemu-x86_64_defconfig HOSTCC="${HOST_CC}" HOSTCFLAGS="-I${TOOLCHAIN}/include" HOSTLDFLAGS="${HOST_LDFLAGS}"
