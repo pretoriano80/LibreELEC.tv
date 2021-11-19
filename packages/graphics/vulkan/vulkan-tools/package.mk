61eb35e48bb009881a3fb7ed82fa672232fdc659
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-19 11:05:04 +0100

    vulkan: add initial Vulkan packages v1.2.199

---
+++ packages/graphics/vulkan/vulkan-tools/package.mk
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="vulkan-tools"
+PKG_VERSION="1.2.199"
+PKG_SHA256="cda034e5990aa92848bfd98045ae77000a789e37b2080a97d4cd7fbb3a089580"
+PKG_LICENSE="Apache-2.0"
+PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
+PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain vulkan-loader"
+PKG_LONGDESC="This project provides Khronos official Vulkan Tools and Utilities."
+
+pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_VULKANINFO=ON \
+                         -DBUILD_ICD=OFF \
+                         -DINSTALL_ICD=OFF \
+                         -DBUILD_WSI_DIRECTFB_SUPPORT=OFF \
+                         -Wno-dev"
+
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_CUBE=ON \
+                             -DBUILD_WSI_XCB_SUPPORT=ON \
+                             -DBUILD_WSI_XLIB_SUPPORT=ON \
+                             -DBUILD_WSI_WAYLAND_SUPPORT=OFF \
+                             -DCUBE_WSI_SELECTION=XCB"
+  elif [ "${DISPLAYSERVER}" = "weston" ]; then
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_CUBE=ON \
+                             -DBUILD_WSI_XCB_SUPPORT=OFF \
+                             -DBUILD_WSI_XLIB_SUPPORT=OFF \
+                             -DBUILD_WSI_WAYLAND_SUPPORT=ON
+                             -DCUBE_WSI_SELECTION=WAYLAND"
+  else
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_CUBE=ON \
+                             -DBUILD_WSI_XCB_SUPPORT=OFF \
+                             -DBUILD_WSI_XLIB_SUPPORT=OFF \
+                             -DBUILD_WSI_WAYLAND_SUPPORT=OFF \
+                             -DCUBE_WSI_SELECTION=DISPLAY"
+  fi
+}
+
+pre_make_target() {
+  # Fix cross compiling
+  find ${PKG_BUILD} -name flags.make -exec sed -i  "s:isystem :I:g" \{} \;
+  find ${PKG_BUILD} -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+}
