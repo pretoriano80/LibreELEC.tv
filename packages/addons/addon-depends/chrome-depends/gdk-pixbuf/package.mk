4291b67cbae6afa0e87f325d0ef73bc3c2c990c3
SupervisedThinking <supervisedthinking@gmail.com>
2021-11-22 17:13:44 +0100

    gdk-pixbuf: don't depend on X11

--- packages/addons/addon-depends/chrome-depends/gdk-pixbuf/package.mk
+++ packages/addons/addon-depends/chrome-depends/gdk-pixbuf/package.mk
@@ -8,7 +8,7 @@ PKG_SHA256="c4a6b75b7ed8f58ca48da830b9fa00ed96d668d3ab4b1f723dcf902f78bde77f"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.gtk.org/"
 PKG_URL="https://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/${PKG_VERSION:0:4}/gdk-pixbuf-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain glib libjpeg-turbo libpng jasper shared-mime-info tiff libX11"
+PKG_DEPENDS_TARGET="toolchain glib libjpeg-turbo libpng jasper shared-mime-info tiff"
 PKG_DEPENDS_CONFIG="shared-mime-info"
 PKG_LONGDESC="GdkPixbuf is a a GNOME library for image loading and manipulation."
 
